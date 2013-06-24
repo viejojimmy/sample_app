6aae3030950a46b7649f77d8987a66a6f94319bb
Jimmy Henriquez <viejojimmy@hotmail.com>
2013-06-24 19:13:13 -0400

    Add user following

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -6,4 +6,22 @@ class Micropost < ActiveRecord::Base
   validates :content, presence: true, length: { maximum: 140 }
   
   default_scope order: 'microposts.created_at DESC'
+  
+  # Basic query uses followed_user_ids as a Rails default to get an ID array (ids) from
+  #the followed_users method.
+  #def self.from_users_followed_by(user)
+  #  followed_user_ids = user.followed_user_ids
+  #  where("user_id IN (?) OR user_id = ?", followed_user_ids, user)
+  #end
+  
+  # A better implementation with subsets
+  # Returns microposts from the users being followed by the given user.
+  def self.from_users_followed_by(user)
+    followed_user_ids = "SELECT followed_id FROM relationships
+                         WHERE follower_id = :user_id"
+    where("user_id IN (#{followed_user_ids}) OR user_id = :user_id",
+          user_id: user.id)
+  end
+  
+  
 end
--- spec/requests/micropost_pages_spec.rb
+++ spec/requests/micropost_pages_spec.rb
@@ -37,5 +37,34 @@ describe "Micropost Pages" do
       end
     end
   end
+    
+  describe "micropost delete of another user" do
+        let(:other_user) { FactoryGirl.create(:user) }
+        before do
+          user.follow!(other_user)
+          FactoryGirl.create(:micropost, user: other_user, content: "Foo")
+          visit root_path 
+        end
+ 
+        it { should have_content("Foo") }
+        it { should_not have_link('delete') }
+  end
 
+  describe "pagination" do
+      before(:all) { 31.times  { FactoryGirl.create(:micropost, user: user, content: "Foo") } }
+      after(:all)  { Micropost.delete_all }   
+      
+      before { visit root_path }
+      
+      it { should have_selector('div.pagination') } 
+      
+      it { should have_content("31 microposts") }
+     
+      it "should list each micropost" do
+          Micropost.paginate(page: 1).each do |micropost|
+            page.should have_selector("li##{micropost.id}", text: micropost.content)
+          end
+      end
+        
+  end
 end
